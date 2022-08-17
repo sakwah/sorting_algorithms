5c561fd20cad59f8729725453764fcdfb1eb870c
sakwah <msakwahm@gmail.com>
2022-08-17 15:31:00 +0000

    betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,26 +12,26 @@ void shell_sort(int *array, size_t size)
 	int interval = 1;
 	int i = 0;
 
-	while(interval <= (int) size/3)
-		interval = interval*3 +1;
+	while (interval <= (int) size / 3)
+		interval = interval * 3 + 1;
 
-	while(interval > 0)
+	while (interval > 0)
 	{
-		for(outer = interval; outer < (int) size; outer++)
+		for (outer = interval; outer < (int) size; outer++)
 		{
 			valueToInsert = array[outer];
 			inner = outer;
 
-			while(inner > interval -1 && array[inner - interval] >= valueToInsert)
+			while (inner > interval - 1 && array[inner - interval] >= valueToInsert)
 			{
 				array[inner] = array[inner - interval];
-				inner -=interval;
+				inner -= interval;
 			}
 
 			array[inner] = valueToInsert;
 		}
 
-		interval = (interval -1) /3;
+		interval = (interval - 1) / 3;
 		print_array(array, size);
 		i++;
 	}
